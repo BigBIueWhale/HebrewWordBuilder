57d91f9900871d49b102e2a7f2a9fb2962f65867
BigBIueWhale <rzyroff@gmail.com>
2021-06-23 00:57:54 +0300

    Add project files.

---
+++ HebrewWordbuilder/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿
+namespace HebrewWordbuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(366, 65);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(305, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "נא לכתוב את האותיות כאן:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(373, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(292, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(598, 225);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(187, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "מצא מילים";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 282);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(773, 165);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "תוצאה";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 12);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(380, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "תוכנה זו נכתבה על ידי רונן זירוף. התוכנה היא תחת רישיון The Unlicense";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(111, 112);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox1.Size = new System.Drawing.Size(217, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "הבדל בין אותיות סופיות ללא סופיות";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(103, 213);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox3.Size = new System.Drawing.Size(225, 17);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "התייחס לסימנים בקלט שהם לא אותיות";
+            this.checkBox3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(111, 9);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(137, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "כאן יופיעו השגיאות";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(134, 236);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox2.Size = new System.Drawing.Size(194, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "השתמש באותיות יותר מפעם אחת";
+            this.checkBox2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(76, 259);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox4.Size = new System.Drawing.Size(252, 17);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "הכלל גם תוצאות עם סימנים שהם לא אותיות";
+            this.checkBox4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(96, 135);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Enabled = false;
+            this.numericUpDown2.Location = new System.Drawing.Point(96, 159);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown2.TabIndex = 15;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Location = new System.Drawing.Point(150, 137);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(184, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "מספר מינימלי של אותיות בתוצאה:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Location = new System.Drawing.Point(146, 161);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(188, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "מספר מקסימלי של אותיות בתוצאה:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label6.Enabled = false;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(219, 85);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(113, 24);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "פחות תוצאות:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Enabled = false;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(221, 186);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(107, 24);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "יותר תוצאות:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 59);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "שנה הגדרות";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "WordBuilder Hebrew";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HebrewWordbuilder/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace HebrewWordbuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class HebrewWord
+        {
+            public HebrewWord(string word, int numOccurences)
+            {
+                this.word = word;
+                this.numOccurences = numOccurences;
+            }
+            public string word;
+            public int numOccurences;
+            public static string HebrewLetters = "אבגדהוזחטיכךלמםנןסעפףצץקרשת";
+            public static bool contains_non_hebrew_non_space(string str_to_check)
+            {
+                foreach (char ch in str_to_check)
+                {
+                    if ((ch != ' ') && (!HebrewWord.HebrewLetters.Contains(ch)))
+                        return true;
+                }
+                return false;
+            }
+            public static char convert_to_not_ending_letter(char ch)
+            {
+                if (ch == 'ך')
+                    return 'כ';
+                if (ch == 'ם')
+                    return 'מ';
+                if (ch == 'ן')
+                    return 'נ';
+                if (ch == 'ף')
+                    return 'פ';
+                if (ch == 'ץ')
+                    return 'צ';
+                return ch;
+            }
+        };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int max_num_of_words_for_result = 30000;
+            button1.Enabled = false;
+            bool differentiate_ending_letters = checkBox1.Checked;
+            bool respect_non_hebrew_symbols = checkBox3.Checked;
+            bool allow_using_letters_more_than_once = checkBox2.Checked;
+            bool include_results_with_non_letters = checkBox4.Checked;
+            int min_number_of_letters = (int)numericUpDown1.Value;
+            int max_number_of_letters = (int)numericUpDown2.Value;
+            if (min_number_of_letters > max_number_of_letters)
+            {
+                numericUpDown1.Value = 1;
+                numericUpDown2.Value = 400;
+                min_number_of_letters = (int)numericUpDown1.Value;
+                max_number_of_letters = (int)numericUpDown2.Value;
+            }
+            label3.Text = "כאן יופיעו השגיאות";
+            label3.ForeColor = Color.Blue;
+            label3.BackColor = Color.Yellow;
+            label3.Refresh();
+            string input_data = textBox1.Text;
+            List<HebrewWord> valid_words = new List<HebrewWord>();
+            string textfile_path = "words.txt";
+            if (File.Exists(textfile_path))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(textfile_path);
+                int lines_counter = 0;
+                foreach (string line in lines)
+                {
+                    int index_of_comma = line.IndexOf(",");
+                    if (index_of_comma != -1)
+                    {
+                        bool was_error_in_parse = false;
+                        int number_of_occurences = 0;
+                        try
+                        {
+                            number_of_occurences = Int32.Parse(line.Substring(index_of_comma + 2, line.Length - (index_of_comma + 2)));
+                        }
+                        catch (FormatException)
+                        {
+                            was_error_in_parse = true;
+                        }
+                        if (!was_error_in_parse)
+                        {
+                            string current_word_str = line.Substring(0, index_of_comma);
+                            HebrewWord current_word = new HebrewWord(current_word_str, number_of_occurences);
+                            string input_data_cpy = "";
+                            if (!differentiate_ending_letters)
+                            {
+                                for (int char_input_data_counter = 0; char_input_data_counter < input_data.Length; ++char_input_data_counter)
+                                    input_data_cpy += HebrewWord.convert_to_not_ending_letter(input_data[char_input_data_counter]);
+                            }
+                            else
+                                input_data_cpy = input_data;
+                            bool word_is_valid = true;
+                            while (current_word_str.Length > 0)
+                            {
+                                char current_char_in_word = current_word_str[0];
+                                if (!differentiate_ending_letters)
+                                    current_char_in_word = HebrewWord.convert_to_not_ending_letter(current_char_in_word);
+                                if (respect_non_hebrew_symbols || HebrewWord.HebrewLetters.Contains(current_char_in_word))
+                                {
+                                    if (input_data_cpy.Contains(current_char_in_word))
+                                    {
+                                        if (!allow_using_letters_more_than_once)
+                                            input_data_cpy = input_data_cpy.Remove(input_data_cpy.IndexOf(current_char_in_word), 1);
+                                    }
+                                    else
+                                    {
+                                        word_is_valid = false;
+                                        break;
+                                    }
+                                }
+                                current_word_str = current_word_str.Remove(0, 1);
+                            }
+                            if (word_is_valid)
+                            {
+                                if (valid_words.Count >= max_num_of_words_for_result)
+                                {
+                                    label3.Text = "שגיאה: יש יותר מדי תוצאות.";
+                                    label3.ForeColor = Color.Green;
+                                    label3.BackColor = Color.Pink;
+                                    break;
+                                }
+                                if ((include_results_with_non_letters
+                                    || (!HebrewWord.contains_non_hebrew_non_space(current_word.word)))
+                                    && ((min_number_of_letters <= current_word.word.Length) && (max_number_of_letters >= current_word.word.Length)))
+                                    valid_words.Add(current_word);
+                            }
+                        }
+                        else
+                        {
+                            label3.Text = "שגיאה המספר בקובץ בשורה " + lines_counter.ToString() + " הוא לא תקין";
+                            label3.ForeColor = Color.Green;
+                            label3.BackColor = Color.Pink;
+                            break;
+                        }
+                    }
+                    ++lines_counter;
+                }
+                valid_words.Sort((a, b) => b.numOccurences.CompareTo(a.numOccurences));
+                string text_of_answer = "";
+                int num_valid_word_counter = 1;
+                foreach (HebrewWord hebrew_word in valid_words)
+                {
+                    text_of_answer += num_valid_word_counter.ToString() + ". " + hebrew_word.word + "\r\n";
+                    ++num_valid_word_counter;
+                }
+                if (valid_words.Count == 0)
+                {
+                    label3.Text = "אין תוצאות.";
+                    label3.ForeColor = Color.Green;
+                    label3.BackColor = Color.Pink;
+                }
+                textBox2.Text = text_of_answer;
+            }
+            else
+            {
+                string appPath = Application.StartupPath;
+                label3.Text = "שגיאה: הקובץ words.txt לא נמצא בתיקייה" + "\r\n" + appPath;
+                label3.ForeColor = Color.Green;
+                label3.BackColor = Color.Pink;
+                textBox2.Text = "תוצאה";
+            }
+            button1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            label6.Enabled = true;
+            label7.Enabled = true;
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            checkBox3.Enabled = true;
+            checkBox4.Enabled = true;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            label4.Enabled = true;
+            label5.Enabled = true;
+        }
+    }
+}
